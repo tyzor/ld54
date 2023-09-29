f8e8404a56584538d73ec969e8468ea1fb54500c
pwrtoppl <pwrtoppl@gmail.com>
2023-09-29 17:35:19 -0600

    new approach, working

--- Assets/CameraControl.cs
+++ Assets/CameraControl.cs
@@ -3,14 +3,22 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public Transform player;
-    public Vector3 offset;
-    public float lookSpeed = 700f;
+    public float mouseSensitivity = 100f;
+    private float xRotation = 0f;
 
     void Update()
     {
-        transform.position = player.position + offset;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        float turnInput = Input.GetAxis("Horizontal") * lookSpeed * Time.deltaTime;
-        player.Rotate(Vector3.up * turnInput);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);  // Clamps the vertical rotation to prevent flipping
+
+        // Apply rotation to player for left and right movement
+        player.Rotate(Vector3.up * mouseX);
+        // Set the camera's rotation directly for up and down movement
+        transform.localRotation = Quaternion.Euler(xRotation, player.eulerAngles.y, 0f);
+
+        transform.position = player.position;  // Keeps the camera at the player's position
     }
 }
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,14 +3,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float turnSpeed = 700f;
+    private Transform cameraTransform;
+
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+    }
 
     void Update()
     {
-        float moveInput = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        float turnInput = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+        float horizontalInput = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float verticalInput = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
-        transform.Translate(Vector3.forward * moveInput);
-        transform.Rotate(Vector3.up * turnInput);
+        Vector3 forward = cameraTransform.forward;
+        forward.y = 0f;  // Ignore vertical component of camera's forward vector
+        forward.Normalize();
+
+        Vector3 right = cameraTransform.right;
+        right.y = 0f;  // Ignore vertical component of camera's right vector
+        right.Normalize();
+
+        Vector3 moveDirection = (forward * verticalInput) + (right * horizontalInput);
+        transform.Translate(moveDirection, Space.World);
     }
 }
